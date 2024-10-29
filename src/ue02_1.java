992859f9ed962ed7e910d1ae36b6dec3d66c2062
tristan0704 <tristan.trunez@outlook.de>
2024-10-29 13:45:58 +0100

    Hausübung NR2 Tristan Trunez

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -3,7 +3,7 @@ import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-
+//Beispiel 1
      int num1 = In.readInt();
      int num2 = In.readInt();
      int num3 = In.readInt();
